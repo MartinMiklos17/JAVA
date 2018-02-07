cdc853a68e50284703394292bd14f368e987a87b
MartinMiklos17 <35778155+MartinMiklos17@users.noreply.github.com>
2018-02-07 12:42:51 +0100

    Add files via upload

---
+++ Elvis/src/elvis/Elvis.java
@@ -0,0 +1,27 @@
+package elvis;
+
+public class Elvis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int weight = 250;
+        System.out.println("Elvis's weight is " + weight);
+        System.out.println("Elvis eats a lot");
+        weight = weight + 10;
+        System.out.println("Elvis is now " + weight + " kg");
+        System.out.println("Elvis eats a lot");
+        weight = weight + 10;
+        System.out.println("Elvis is now " + weight + " kg");
+        System.out.println("Elvis eats a lot");
+        weight = weight + 10;
+        System.out.println("Elvis is now " + weight + " kg");
+        System.out.println("Elvis eats a lto");
+        weight = weight +100;
+        System.out.println("Elvis is now " + weight + " kg");
+        System.out.println("Elvis now" + weight  + " kg");
+        
+    }
+    
+}
